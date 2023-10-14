4e9dcf4746b5fe3e47bf46129596def259c49ca6
Andrew-Vick <vickandrew093@gmail.com>
2023-10-14 15:08:06 -0500

    added style to product-details

--- MidtermProject/link.js
+++ MidtermProject/link.js
@@ -8,13 +8,16 @@ function showProductPopup(productId) {
         .then(response => response.json())
         .then(products => {
             const product = products.find(p => p.id == productId);
+
             if (product) {
                 const modalContent = document.getElementById('productModalContent');
                 modalContent.innerHTML = `
+                    <img class="product-img" src="${product.image}" alt="${product.name}"></img>
                     <h2>${product.name}</h2>
+                    <p>${product.price}</p>
                     <p>${product.description}</p>
                 `;
-                
+
                 $('#productModal').modal('show');
             }
         });
